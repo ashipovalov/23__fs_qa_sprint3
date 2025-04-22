65a659758570bac83994b8132bccc98393ab58d8
Aleksandr Shipovalov <ashipovalov@gmail.com>
2025-04-22 20:33:14 +0300

    demo rebase

--- src/main/java/ru/practicum/SpeedLight.java
+++ src/main/java/ru/practicum/SpeedLight.java
@@ -5,6 +5,7 @@ public class SpeedLight {
     public String showLight(int currentSpeed) {
         System.out.println("Эта строка для демонстрации");
         System.out.println("Это наше самое настоящее изменение");
+        System.out.println("А теперь посмотрим ребейз");
         if (currentSpeed < 60) {
             return "green";
         } else if (currentSpeed < 80) {
