1ee85eba0517a2a6ca17476900482bb2c04713ca
ashipovalov <ashipovalov@gmail.com>
2025-04-22 20:28:17 +0300

    Хочу коммитить

--- src/main/java/ru/practicum/SpeedLight.java
+++ src/main/java/ru/practicum/SpeedLight.java
@@ -3,6 +3,7 @@ package ru.practicum;
 
 public class SpeedLight {
     public String showLight(int currentSpeed) {
+        System.out.println("Это наше самое настоящее изменение");
         if (currentSpeed < 60) {
             return "green";
         } else if (currentSpeed < 80) {
