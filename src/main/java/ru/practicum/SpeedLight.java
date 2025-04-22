a228488fff7db3dfbac41830aabc3e336c2ae93d
Aleksandr Shipovalov <ashipovalov@gmail.com>
2025-04-22 20:26:52 +0300

    demo1

--- src/main/java/ru/practicum/SpeedLight.java
+++ src/main/java/ru/practicum/SpeedLight.java
@@ -3,6 +3,7 @@ package ru.practicum;
 
 public class SpeedLight {
     public String showLight(int currentSpeed) {
+        System.out.println("Эта строка для демонстрации");
         if (currentSpeed < 60) {
             return "green";
         } else if (currentSpeed < 80) {
